78960de78b32365663bf425db8dd436e79c05bf5
2276225819 <2276225819@qq.com>
2017-04-15 20:12:31 +0800

    init

---
+++ assets/view.php
@@ -0,0 +1,87 @@
+ <html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style> 
+        html,body{margin: 0; height: 100%;}
+        pre{margin:0px; font-family:'Microsoft YaHei'} 
+        ul{ padding-left:20px;   }
+        a{ text-decoration: none; color: inherit;}  
+        .table tr{ background:#F5F5F5; }
+        .table tr+tr{background:#fff} 
+        .table td{border:1px solid #c0c0c0; vertical-align: top;}
+        .table td{padding:8px; white-space:pre} 
+        .table { border-collapse: collapse; border-spacing: 0;margin:10px 2px;  } 
+     
+        .left{ height: 100%; overflow: auto; float: left; width: 250px; line-height: 32px; padding-left:10px;}
+        .right{height: 100%; overflow: auto; padding: 0 10px;   } 
+        .right:after{ content:''; height:500px; display:block;}
+    </style>
+    <title><?=$title?></title>
+</head>
+<body >   
+    <div class="left" > 
+        <?php $tfn=function($tree)use(&$tfn){
+        echo "<ul>"; 
+        foreach ($tree as $key=>$value) {
+            echo "<li>";
+            if(is_array($value)){
+                echo $key;
+                $tfn($value);  
+            } 
+            else {
+                echo "<a href='#$value'>$key</a>";
+            }
+            echo "</li>";
+        }
+        echo "</ul>";           
+        };$tfn($tree);?> 
+    </div>
+    <div class="right" >
+        <?php foreach($apis as $api){ ?>
+        <div class="bb">
+            <a style="display:block;height:1px;overflow:hidden;" name="<?=join($api['name'],'-')?>">*</a>
+            <h2><?=end($api['name'])?></h2>
+            <?php if(isset($api['desc'])){?>
+            <h3><?=$api['desc']?></h3>
+            <?php } ?>
+             <?php if(isset($api['url'])){?>
+            <fieldset>
+                <legend>请求：</legend>
+                <pre><?=$api['url']??''?></pre>
+            </fieldset> 
+            <?php } ?>
+            <?php if(isset($api['req'])){ ?>
+            <table class="table">
+                <tr><td width="120">请求字段</td> <td width="100">参考值</td> <td >说明</td></tr>
+                <?php foreach($api['req'] as $key=>$req){?>
+                <tr><td width="120"><?=$key?></td><td><?=$req[0]??''?></td><td><?=$req[1]??''?></td></tr>
+                <?php } ?>
+            </table> 
+            <?php } ?>
+            <?php foreach($api['success']??[] as $success){?>
+            <fieldset>
+                <legend>响应成功：<?=$success[0]?></legend>
+                <pre><?=$success[1]?></pre>
+            </fieldset>
+            <?php } ?>
+            <?php foreach($api['error']??[] as $error){?>
+            <fieldset>
+                <legend>响应失败：<?=$error[0]?></legend>
+                <pre><?=$error[1]?></pre>
+            </fieldset>
+            <?php } ?> 
+            <?php if(isset($api['res'])){ ?>
+            <table class="table">
+                <tr><td width="120">响应字段</td> <td width="100">参考值</td> <td >说明</td></tr>
+                <?php foreach($api['res']  as $key=>$res){?>
+                <tr><td width="120"><?=$key?></td><td><?=$res[0]??''?></td><td><?=$res[1]??''?></td></tr>
+                <?php } ?>
+            </table> 
+            <?php } ?>
+        </div>
+        <?php } ?> 
+    </div>
+</body>
+</html>
---
+++ src/PHPApi.php
@@ -0,0 +1,70 @@
+<?php namespace xlx;
+class PHPApi{ 
+    public $title="PHPAPI";
+    public $paths=[]; 
+    public $exts=['*.php'];
+    public $define=[];
+    function dirs($dir,&$arr){
+        if(!is_dir($dir)) return $arr[] = $dir; 
+        foreach ($this->exts as $ext){
+            foreach ( glob($dir.'/'.$ext) as $value){ 
+                $this->dirs($value,$arr); 
+            }  
+        }    
+    }
+    function bulid($doc,&$apis){
+        preg_match_all('/[\r\n]@(\S+)\s([\S\s]+?)(?=[\r\n]@|$)/',$doc,$arr,PREG_SET_ORDER);
+        foreach ($arr as &$v){ 
+            array_shift($v); 
+            if($v[0]=='api')$api['url']=$v[1];
+            if($v[0]=='apiName')$api['name']=array_filter(preg_split('/\s+/',$v[1]));
+            if($v[0]=='apiDesc')$api['desc']=$v[1];
+            if($v[0]=='apiUse'){ 
+                $this->bulid("\n".$this->define[$v[1]],$a); 
+                $api = array_merge($api??[],$a[0]); 
+            }
+            if($v[0]=='reqParam'){
+                $a = preg_split('/\s+/',$v[1],3);
+                $api['req'][array_shift($a)]=$a; 
+            } 
+            if($v[0]=='resParam'){
+                $a = preg_split('/\s+/',$v[1],3);
+                $api['res'][array_shift($a)]=$a; 
+            }
+            if($v[0]=='resSuccess')
+                $api['success'][]= preg_split('/[\r\n]/',$v[1],2);
+            if($v[0]=='resError')
+                $api['error'][]= preg_split('/[\r\n]/',$v[1],2);
+        }
+        if(!empty($api))
+            $apis[]=$api; 
+    }
+    public function gen($return=false){ 
+        $apis=[];
+        foreach ($this->paths as $path)  
+            $this->dirs($path,$files); 
+        foreach ($files as $value){
+            $txt=file_get_contents($value);
+            preg_match_all('/\/\*[\S\s]+?[\S\s]+?\*\//',$txt,$arr);
+            foreach ($arr[0] as $txt) {
+                if(strstr($txt,'@api')){
+                    $txt = substr($txt,3,strlen($txt)-5); 
+                    $txt = preg_replace('/([\r\n])\s+\*\s/',"\n",$txt);
+                    $this->bulid($txt,$apis);  
+                }
+            } 
+        } 
+        return $return?$apis:$this->view($apis);
+    }  
+    public function view($apis){ 
+        $title=$this->title;
+        $tree=[];
+        foreach ($apis as  $value) {
+            $t = &$tree;
+            foreach ($value['name'] as $k) 
+                $t = &$t[$k]; 
+            $t=join($value['name'],'-');
+        } 
+        include __DIR__.'/../assets/view.php';
+    }
+}  
